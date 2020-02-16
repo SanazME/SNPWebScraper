18291c4c9862d24166410de7d6f68f67bf879973
Sanaz <sanaz.azh@gmail.com>
2020-02-16 11:55:08 -0500

    redirect stdout to stderr to see outputs for the build step

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
-docker image build -t snpedia:1.0 . > ./build.log # send the build output to a build.log file
+docker image build -t snpedia:1.0 . >&2 # send the stdout to stderr
 docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app snpedia:1.0 ./runInDocker.sh
